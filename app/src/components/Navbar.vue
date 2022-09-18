52458c2d3f02ac1d28b9b1518c3057fc59c3de7e
Shelton Louis <louisshelton0@gmail.com>
2022-09-18 18:49:31 -0400

    Created the template

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,24 +1,22 @@
 <script lang="tsx" setup>
 import useSideBarStore from "@/stores/useSideBarStore";
 import { NavLinks } from "@/types";
-import Container from "./Container.vue";
 import LightDarkSwitch from "./LightDarkSwitch.vue";
 import Sidebar from "./Sidebar.vue";
 
 const { setIsShownToTrue } = useSideBarStore();
 
-const links: ReadonlyArray<NavLinks> = [
-  { path: "/", text: "Home" },
-  { path: "/remark-showcase", text: "Showcase" },
-];
+const links: ReadonlyArray<NavLinks> = [{ path: "/", text: "Home" }];
 </script>
 
 <template>
   <nav class="bg-blue-600 dark:bg-blue-300 text-gray-50 dark:text-gray-900">
-    <Container class="w-5/6 max-w-screen-xl">
+    <div class="w-5/6 max-w-screen-xl">
       <div data-padding-layer class="py-2 px-6">
         <div data-content-layer class="flex justify-around">
-          <div data-hamburger-menu @click="setIsShownToTrue"></div>
+          <div data-hamburger-menu @click="setIsShownToTrue">
+            <HambugerIcon />
+          </div>
           <div class="w-2/5 lg:w-4/5">
             <div data-padding-layer class="py-2 px-6">
               <ul data-content-layer class="flex gap-4 items-center">
@@ -35,27 +33,25 @@ const links: ReadonlyArray<NavLinks> = [
           <LightDarkSwitch />
         </div>
       </div>
-    </Container>
+    </div>
   </nav>
 
   <Sidebar :links="links" />
 </template>
 
 <script lang="tsx">
-function HambugerIcon() {
-  return (
-    <svg viewBox="0 0 24 24" class="w-12 h-full">
-      <path
-        fill="currentColor"
-        d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
-      ></path>
-    </svg>
-  );
-}
-
 export default {
   components: {
-    HambugerIcon,
+    HambugerIcon() {
+      return (
+        <svg viewBox="0 0 24 24" class="w-12 h-full">
+          <path
+            fill="currentColor"
+            d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+          ></path>
+        </svg>
+      );
+    },
   },
 };
 </script>
